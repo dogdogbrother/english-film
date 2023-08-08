338ae2b9a7ec3ae77ec3f1b98fbbe06ac0e9e517
王浩 <wanghao@wanghaodeMac-mini.local>
2023-08-08 18:54:35 +0800

    背单词功能开发好了

--- src/component/phonetic/index.tsx
+++ src/component/phonetic/index.tsx
@@ -1,44 +1,67 @@
 import type { TranslateProp } from '@/api/word'
 import styled from '@emotion/styled'
+import { VolumeNotice, VolumeSmall} from '@/component/icons/volume'
+import { useRef, useState } from 'react'
 
 interface Props {
   translate: TranslateProp
 }
 function Phonetic(props: Props) {
   const { translate } = props
+  const videoNotice = useRef(new Audio())
+  const [ blinkType, setBlinkType ] = useState(true)
+  const [ playType, setPlayType ] = useState<'uk' | 'us'>()
+  const timer = useRef<any>()
+  videoNotice.current.addEventListener('ended', () => {
+    clearInterval(timer.current)
+    setPlayType(undefined)
+  })
+  function playPhonetic(speech: string, type: 'uk' | 'us') {
+    // 正在播放中
+    if (!videoNotice.current.paused) return
+    setPlayType(type)
+    videoNotice.current.src = speech
+    videoNotice.current.play()
+    timer.current = setInterval(() => {
+      setBlinkType(blinkType => !blinkType)
+    }, 400)
+  }
   return <PhoneticBox>
-    <div onClick={() => playPhonetic(translate.ukSpeech)}>
+    <div onClick={() => playPhonetic(translate.ukSpeech, 'uk')}>
       <span>英 | </span>
-      <span>{translate.ukPhonetic}</span>
-      <div className='phonetic-icon'>1</div>
+      <span style={{marginRight: '5px'}}>{translate.ukPhonetic}</span>
+      {
+        blinkType && playType === 'uk' ? <VolumeNotice width='1.2em' height='1.2em' /> : <VolumeSmall width='1.2em' height='1.2em' />
+      }
     </div>
-    <div onClick={() => playPhonetic(translate.usSpeech)}>
+    <div onClick={() => playPhonetic(translate.usSpeech, 'us')}>
       <span>美 | </span>
-      <span>{translate.usPhonetic}</span>
-      <div className='phonetic-icon'>1</div>
+      <span style={{marginRight: '5px'}}>{translate.usPhonetic}</span>
+      {
+        blinkType && playType === 'us' ? <VolumeNotice width='1.2em' height='1.2em' /> : <VolumeSmall width='1.2em' height='1.2em' />
+      }
     </div>
   </PhoneticBox>
 }
 const PhoneticBox = styled.div`
   display: flex;
   font-size: 14px;
-  color: #666;
   > div {
     margin: 0 10px;
     cursor: pointer;
     display: flex;
     align-items: center;
+    color: #666;
     &:active {
       color: #333;
     }
   }
   .phonetic-icon {
     margin-left: 5px;
+    width: 20px;
+    height: 20px;
+    background-size: cover;
   }
 `
-// 发音
-function playPhonetic(speech: string) {
-  const audio = new Audio(speech)
-  audio.play()
-}
+
 export default Phonetic
--- src/pages/memory/index.tsx
+++ src/pages/memory/index.tsx
@@ -1,29 +1,76 @@
 import { observer } from "mobx-react-lite"
 import wordStore from '@/store/word'
-import { useState, useRef, useMemo } from 'react'
+import { useState, useMemo } from 'react'
 import styled from '@emotion/styled'
-import { Empty } from 'antd'
+import { Empty, Button } from 'antd'
 import Phonetic from '@/component/phonetic'
+import { DeleteOutlined } from "@ant-design/icons"
+import { delCollect } from '@/api/word'
 
 function Memory() {
-  const wordIndex = useRef(0)
+  const [wordIndex, setWordIndex] = useState(0)
   const wordList = useMemo(() => {
     return [...wordStore.collectList].sort(() => 0.5 - Math.random()) 
   }, [wordStore.collectList.length])
+  const [ delWord, setDelWord ] = useState<string[]>([])
+  const [ showTranslate, setShowTranslate ] = useState(false)
   const currentWord = useMemo(() => {
-    console.log(wordList[wordIndex.current]);
-    
-    return wordList[wordIndex.current]
+    return wordList[wordIndex]
   }, [wordIndex, wordList])
+  async function _delCollect() {
+    setDelWord(delWord => [...delWord, currentWord.word])
+    delCollect(currentWord.word)
+    nextWord()
+  }
+  function nextWord(_index?: number) {
+    const nextIndex = _index ?? (wordIndex + 1)
+    // 这是删除了所有单词
+    if (delWord.length === wordList.length) return
+    let targetIndex = 0
+    if (nextIndex < wordList.length) {
+      targetIndex = nextIndex
+    }
+    // 如果下一个单词是之前删除过得那么就递归吧
+    if (delWord.find(item => wordList[targetIndex].word === item)) {
+      nextWord(nextIndex)
+    } else {
+      setShowTranslate(false)
+      setWordIndex(targetIndex)
+    }
+  }
   return <WordBookWrap>
     {
-      wordStore.collectList.length === 0
+      wordStore.collectList.length === 0 || wordStore.collectList.length === delWord.length
       ?
-      <Empty description="还没有收藏单词" />
+      <Empty description="没有收藏的单词了" />
       :
       <WordCark>
         <h3 className="word">{currentWord.word}</h3>
         <Phonetic translate={currentWord.youdao} />
+        <div className="translate-content">
+          {
+            showTranslate && <ul className='translate-list'>
+              {
+                currentWord.youdao.explains.map((translate: any, index: number) => <li key={index}>
+                  { translate }
+                </li>)
+              }
+            </ul>
+          }
+        </div>
+        <div className="button-box">
+          <DelButton onClick={() => _delCollect()}>
+            <DeleteOutlined />
+            <span>已掌握,取消收藏</span>
+          </DelButton>
+          <Button 
+            style={{marginBlock: '10px'}} 
+            type='primary' 
+            block 
+            onClick={() => nextWord()}
+          >认识</Button>
+          <Button type='primary' danger block onClick={() => setShowTranslate(true)}>提示一下</Button>
+        </div>
         {/* {wordList[wordIndex.current]} */}
       </WordCark>
     }
@@ -31,10 +78,11 @@ function Memory() {
 }
 
 const WordBookWrap = styled.div`
-  max-width: 1080px;
+  max-width: 680px;
   margin: 0 auto;
   display: flex;
   justify-content: center;
+  height: calc(100vh - 61px);
 `
 const WordCark = styled.div`
   box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
@@ -46,7 +94,42 @@ const WordCark = styled.div`
   flex-direction: column;
   align-items: center;
   .word {
-    font-size: 24px;
+    font-size: 28px;
+  }
+  .translate-content {
+    padding: 10px 10px;
+    flex: 1;
+    li {
+      margin: 10px 0;
+      font-size: 13px;
+      color: #666;
+    }
+  }
+  .button-box {
+    width: 100%;
+    padding-top: 30px;
+    position: relative;
+  }
+`
+const DelButton = styled.div`
+  position: absolute;
+  height: 30px;
+  right: 0;
+  top: 0;
+  padding: 0 15px;
+  border-radius: 10px;
+  background-color: #eee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #666;
+  font-size: 12px;
+  cursor: pointer;
+  &:hover {
+    color: #333;
+  }
+  > * {
+    margin: 0 2px;
   }
 `
 export default observer(Memory)
